1f5f734e77478f1f77a96df1cf874d23b212fb11
tsilavina <andriamiart114@gmail.com>
2024-03-14 15:03:21 +0300

    update test.js

--- level3.js
+++ level3.js
@@ -11,7 +11,7 @@ export function level3(input) {
         }else if(input[i].split(' ').includes("JUMP")){
             let val=parseInt(input[i].split(' ')[1])
             input[i]="X"
-            i+=val
+            val>0?i+=val-1:i+=val
         }else if(input[i]=="X"){
             continue 
         }
