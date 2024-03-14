0f7b47d5409f097e7ccb438bf0a4674c4ac6cb11
dyferherios <freddyandriamanohiniaina@gmail.com>
2024-03-14 07:34:13 -0400

    delete script

--- script.js
+++
@@ -1,4 +0,0 @@
-export function howManyDuplicateAreEvem(arr) {
-    arr = arr.map(x => Math.abs(x)).filter((a) => a%2==0).filter((v, i, arrNow) => arrNow.indexOf(v) !== i).filter((v, i, a) => a.indexOf(v) === i)
-    return arr.length
-}
